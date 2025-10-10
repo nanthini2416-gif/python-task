c89786aa563568937d511d3e0f323eb99e636dd1
nanthini2416-gif <nanthini2416@gmail.com>
2025-10-10 21:04:03 +0530

    elif sem.py

---
+++ elif sem.py
@@ -0,0 +1,11 @@
+year=int(input("Enter The Year"))
+sem=int(input("Enter The Semester"))
+if year==1 and sem==1:
+  print("Tamil,English,Maths")
+elif year==1 and sem==2:
+    print("oops,DBMS,C++")
+elif year==2 and sem==1:
+    print("java,c++,c")
+else:
+    print("Project Works")
+    
