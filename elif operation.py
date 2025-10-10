e3d2441144284ab58c03dd0bc9dfda0affa7cd4b
nanthini2416-gif <nanthini2416@gmail.com>
2025-10-10 21:03:03 +0530

    elif operation.py

---
+++ elif operation.py
@@ -0,0 +1,22 @@
+a=int(input("Enter a value"))
+b=int(input("Enter b value"))
+c=input("Enter operation +,-,*,/")
+if c=='+':
+ print("Addition",a+b)
+elif c=='-':
+ print("Subtraction",a-b)
+elif c=='*':
+ print("Multiplication",a*b)
+
+elif c=='/':
+  if c!=0:
+      print("Division",a/b)
+  else:
+      print("Not Division")
+else:
+    print("Invalid Operator")
+    
+  
+
+
+            
