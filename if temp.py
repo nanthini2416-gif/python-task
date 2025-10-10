b73b6dadccf5f4314ace71cd979b425151e08fbc
nanthini2416-gif <nanthini2416@gmail.com>
2025-10-10 21:18:15 +0530

    if temp.py

---
+++ if temp.py
@@ -0,0 +1,15 @@
+temp=int(input("Enter temprature"))
+if temp<0:
+        print("frezzing Weather")
+elif temp<10:
+        print("Very Cold Weather")
+elif temp<20:
+        print("Cold Weather")
+elif temp<30:
+        print("Normal Weather")
+elif temp<40:
+        print("It's Hot")
+else:
+    print("It's Very Hot")
+    
+
