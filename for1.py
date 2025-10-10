0bfbaee59d9e59181257dd728d5b65b5b7be9d6e
nanthini2416-gif <nanthini2416@gmail.com>
2025-10-10 21:13:44 +0530

    for1.py

---
+++ for1.py
@@ -0,0 +1,10 @@
+sum = 0
+
+print("Numbers divisible by 9 between 100 and 200:")
+
+for i in range(100, 201):
+    if i % 9 == 0:
+        print(i, end=" ")
+        sum += i
+
+print(f"\nSum: {sum}")
